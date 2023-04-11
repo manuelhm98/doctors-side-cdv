5193296330e20f4aba49acffb732ebcd7bea5d94
Manuel Marquez <80608697+manuelhm98@users.noreply.github.com>
2023-04-11 08:43:29 -0600

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
-export const API = "http://localhost:8000/api"
+export const API = "https://seahorse-app-vuzfr.ondigitalocean.app/api"
 
-export const SOCKET_URL = "http://localhost:8000"
+export const SOCKET_URL = "https://seahorse-app-vuzfr.ondigitalocean.app"
 
 //export const API = "http://159.65.219.24:8000/api"
 
